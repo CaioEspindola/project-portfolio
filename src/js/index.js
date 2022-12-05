4f8bd95f53f670871aa03c72bb9fb535fdbe08d1
Caio Espindola <caioespindola@hotmail.com>
2022-12-05 11:54:45 -0300

    refactor: remove old code

--- src/js/index.js
+++ src/js/index.js
@@ -17,23 +17,3 @@ aba2Selecionada.addEventListener("click", () => {
   aba1.classList.add("tab_hide");
   aba2.classList.remove("tab_hide");
 });
-
-/*var aba2Selecionada = document.getElementById('minhas-redes')
-
-aba2Selecionada.addEventListener('click', () => {
-  const aba1 = document.getElementById('informacao-sobre-min')
-  const aba2 = document.getElementById('informacao-minhas-redes')
-
-  aba1.classList.toggle('aba_hide')
-  aba2.classList.toggle('aba_hide')
-})
-
-var aba1Selecionada = document.getElementById('sobre-mim')
-
-aba1Selecionada.addEventListener('click', () => {
-  const aba1 = document.getElementById('informacao-sobre-min')
-  const aba2 = document.getElementById('informacao-minhas-redes')
-
-  aba1.classList.toggle('aba_hide')
-  aba2.classList.toggle('aba_hide')
-})*/
