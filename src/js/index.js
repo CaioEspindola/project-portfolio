2bd3a96adb416ae9b549119b3b157417b8aa5f2d
Caio Espindola <caioespindola@hotmail.com>
2022-12-06 11:41:48 -0300

    Change class name to english

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
-var aba1Selecionada = document.getElementById("sobre-mim");
+var aba1Selecionada = document.getElementById("tab-about-me");
 
 aba1Selecionada.addEventListener("click", () => {
-  const aba1 = document.getElementById("informacao-sobre-min");
-  const aba2 = document.getElementById("informacao-minhas-redes");
+  const aba1 = document.getElementById("information-about-me");
+  const aba2 = document.getElementById("information-social-media");
 
   aba1.classList.remove("tab_hide");
   aba2.classList.add("tab_hide");
 });
 
-var aba2Selecionada = document.getElementById("minhas-redes");
+var aba2Selecionada = document.getElementById("tab-social-media");
 
 aba2Selecionada.addEventListener("click", () => {
-  const aba1 = document.getElementById("informacao-sobre-min");
-  const aba2 = document.getElementById("informacao-minhas-redes");
+  const aba1 = document.getElementById("information-about-me");
+  const aba2 = document.getElementById("information-social-media");
 
   aba1.classList.add("tab_hide");
   aba2.classList.remove("tab_hide");
