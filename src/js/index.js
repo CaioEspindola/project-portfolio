c66d3900341021644c0f0769d86a4d43b14f9768
João Silva <joao.silva@brasilparalelo.com.br>
2022-12-16 14:36:44 -0300

    feat:rewrite files with prettier

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-const aboutContent = document.getElementById("information-about-me");
-const socialContent = document.getElementById("information-social-media");
+const aboutContent = document.getElementById('information-about-me')
+const socialContent = document.getElementById('information-social-media')
 
-const aboutTab = document.getElementById("tab-about-me");
+const aboutTab = document.getElementById('tab-about-me')
 
-aboutTab.addEventListener("click", () => {
-  aboutContent.classList.remove("tab_hide");
-  socialContent.classList.add("tab_hide");
-});
+aboutTab.addEventListener('click', () => {
+  aboutContent.classList.remove('tab_hide')
+  socialContent.classList.add('tab_hide')
+})
 
-const socialTab = document.getElementById("tab-social-media");
+const socialTab = document.getElementById('tab-social-media')
 
-socialTab.addEventListener("click", () => {
-  aboutContent.classList.add("tab_hide");
-  socialContent.classList.remove("tab_hide");
-});
+socialTab.addEventListener('click', () => {
+  aboutContent.classList.add('tab_hide')
+  socialContent.classList.remove('tab_hide')
+})
